bab6adf11311c7093777621c9494050c63feae20
shubham <shubhamaery10@gmail.com>
2017-10-21 23:24:04 +0530

    hero-image (background) created and two buttons?(dropdown and primary)

---
+++ script.js
@@ -0,0 +1,6 @@
+// function dropdownMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+$(document).ready(function () {
+        $('.dropdown-toggle').dropdown();
+    });
